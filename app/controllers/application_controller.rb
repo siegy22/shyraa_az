f2a2b58c1b4ffb81ba9f642eecd9aeb85524fc05
Yves Siegrist <Elektron1c97@gmail.com>
2024-02-25 13:26:24 +0100

    Make it A-Z to start with
    This reverts commit 78738e213c69b2f09a58c7508a00c7c69899d56b.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
   def default_meta_tags
     {
-      title: 'Z-A Challenge von NoWay4u',
+      title: 'Das große A-Z Projekt',
       reverse: true,
       separator: '|',
       keywords: 'noway, noway4u, noway4u_sir, lol, league of legends, challenge',
       canonical: request.original_url,
       noindex: !Rails.env.production?,
       og: {
-        title: 'Z-A Challenge von NoWay4u',
+        title: 'Das große A-Z Projekt',
         type: 'website',
         url: request.original_url,
       }
