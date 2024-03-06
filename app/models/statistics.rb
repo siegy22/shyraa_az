3d5902d202e508446263ae7540b0d8d5c59dca02
Yves Siegrist <Elektron1c97@gmail.com>
2024-03-06 22:32:09 +0100

    Fix starting time

--- app/models/statistics.rb
+++ app/models/statistics.rb
@@ -13,7 +13,7 @@ class Statistics
     (Match.where(win: true).count / Match.count.to_f * 100).round
   end
 
-  START_DATE = Date.parse("2024-02-08")
+  START_DATE = Date.parse("2024-03-01")
   def duration
     (Date.today - START_DATE).to_i.days
   end
