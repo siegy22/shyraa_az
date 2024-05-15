0bf7ba03d5a90130eac7f3ea4351d4cf649a8d5f
Yves Siegrist <Elektron1c97@gmail.com>
2024-05-15 13:01:19 +0200

    Update START_TIME to be today

--- app/models/statistics.rb
+++ app/models/statistics.rb
@@ -13,7 +13,7 @@ class Statistics
     (Match.where(win: true).count / Match.count.to_f * 100).round
   end
 
-  START_DATE = Date.parse("2024-05-09")
+  START_DATE = Date.parse("2024-05-15")
   def duration
     (Date.today - START_DATE).to_i.days
   end
